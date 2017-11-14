ec4165909c61e3a80b6ba278ba3fd16dd35ea449
James Turello <jturello@yahoo.com>
2017-11-13 16:18:37 -0800

    add create_user resolver unit test

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,9 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
-  # queries are just represented as fields
+  # queries are represented as fields
   field :allLinks, !types[Types::LinkType] do
-    # resolve would be called in order to fetch data for that field
-    resolve ->(obj, args, ctx) {
-      Link.all
-    }
+    # resolve is called to fetch the field's data
+    resolve ->(obj, args, ctx) { Link.all }
   end
 end
